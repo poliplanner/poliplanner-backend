9ae38ce1f4f96881a643cd88f0a7b22af75ba829
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2020-04-15 22:58:05 -0400

    integracion con keycloak

--- src/main/java/com/poliplanner/controller/CarreraController.java
+++ src/main/java/com/poliplanner/controller/CarreraController.java
@@ -1,12 +1,10 @@
 package com.poliplanner.controller;
 
 import com.poliplanner.data.CarreraRepository;
-import com.poliplanner.data.SeccionRepository;
 import com.poliplanner.domain.model.Carrera;
-import com.poliplanner.domain.model.Seccion;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import java.security.Principal;
 
 @RestController
 @RequestMapping(path="/carreras", produces="application/json")
